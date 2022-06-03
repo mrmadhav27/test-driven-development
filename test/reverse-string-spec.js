36be130081cffbdd4894b07e6fde556709734ee2
TonyTan <tonytan2193@gmail.com>
2022-06-03 10:45:02 -0700

    reverse string

--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,16 @@
-// Your code here
+const chai = require('chai');
+const expect = chai.expect
+const reverseString = require('../problems/reverse-string');
+
+describe('reverseString()', () => {
+    it('string should be reversed', () => {
+        const expected = 'nuf'
+        const actual = reverseString('fun')
+
+        expect(actual).to.eql(expected);
+    });
+
+    it('should be error if argument type is not a string', () => {
+        expect(() => reverseString(2)).to.throw(TypeError);
+    })
+});
